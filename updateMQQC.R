0c045b442c47dc412ce6ce9814b28415c2ea4a8b
heuselm <heuselmo@gmx.de>
2022-08-22 17:15:09 +0200

    plot tuning

--- updateMQQC.R
+++ updateMQQC.R
@@ -73,6 +73,7 @@ ev_combined$Acq_day = NULL
 ann = fread("runAnnotation.csv")
 nruns = length(unique(ev_combined$Raw_file))
 ev_combined = merge(ev_combined, ann, by = "Raw_file")
+ev_combined[, Acq_day:=as.character(Acq_day)]
 
 # define n latest n QC analyses:
 latest_n = sort(unique(ev_combined$Acq_day), decreasing = T)[1:n_latest]
@@ -89,20 +90,19 @@ ids = rbind(ids.prot[, level:="Protein.groups"],
 names(ids)
 
 p1 = ggplot(ids,
-            aes(x = as.character(Acq_day), y = V1, col = Sample_id, group = Sample_id)) +
+            aes(x = Acq_day, y = V1, col = Sample_id, group = Sample_id)) +
   geom_point() +
   geom_line() +
   ggtitle(paste(MSname, "QC Identifications MaxQuant")) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90)) +
-  #geom_text(aes(x = as.character(Acq_day), y = V1, label=V1), vjust = -2, cex = 4)  +
+  #geom_text(aes(x = Acq_day, y = V1, label=V1), vjust = -2, cex = 4)  +
   ylab("N") +
   facet_wrap(level~LC_meth, scales = "free") +
   theme(axis.text.x=element_text()) + 
   expand_limits(y=0)
 p1 
-ggsave("QC_IDs_lineplots.pdf", width = (nruns/8)+5, limitsize = F)
-dev.off()
+ggsave("QC_IDs_lineplots.pdf", width = (nruns/8)+3, limitsize = F)
 htmlwidgets::saveWidget(ggplotly(p1), file = "QC_IDs_lineplots.html")
 # ggplotly(p1)
 
@@ -110,11 +110,12 @@ htmlwidgets::saveWidget(ggplotly(p1), file = "QC_IDs_lineplots.html")
 #+ r Mass errors
 names(ev_combined)
 p2 = ggplot(ev_combined, aes(text = Raw_file)) +
-  geom_boxplot(aes(x = as.character(Acq_day), y = Mass_error_ppm, color = Sample_id, group = Raw_file)) +
+  geom_boxplot(aes(x = Acq_day, y = Mass_error_ppm, color = Sample_id, group = Raw_file), outlier.shape = NA) +
   theme_minimal() + 
   theme(axis.text.x = element_text(angle = 90)) +
   # facet_grid(~Acq_day,  scales = "free_x") +
-  geom_hline(yintercept = 0, lty = 2) +
+  geom_hline(yintercept = 0, lty = 1) +
+  geom_hline(yintercept = c(-5,5), lty = 2) +
   ggtitle(paste(MSname, "Mass errors [ppm]"))
 p2
 ggsave("QC_Mass_error_ppm.pdf", height = length(unique(ev_combined$LC_meth))*3, width = (ndirs/4)+3)
@@ -125,12 +126,12 @@ htmlwidgets::saveWidget(ggplotly(p2), file = "QC_Mass_error_ppm.html")
 #+ r ID over RT density plots
 names(ev_combined)
 p3 = ggplot(ev_combined[Acq_day %in% latest_n]) +
-  geom_density(aes(x = Retention_time, y = ..count.., color = as.character(Acq_day), group = Raw_file), adjust = 0.2) +
+  geom_density(aes(x = Retention_time, y = ..count.., color = Acq_day, group = Raw_file), adjust = 0.2) +
   theme_minimal() + 
   facet_grid(~LC_meth, scales = "free") +
   ggtitle(paste0("Identification density along RT", "( ", n_latest, " latest QCs)"))
 p3
-ggsave("QC_ID_density_along_RT.pdf", height = length(unique(ev_combined$LC_meth))*3, width = (ndirs/4)+3)
+ggsave("QC_ID_density_along_RT.pdf", height = length(unique(ev_combined$LC_meth))*3, width = (n_latest)+3)
 htmlwidgets::saveWidget(ggplotly(p3), file = "QC_ID_density_along_RT.html")
 # ggplotly(p3)
 
@@ -145,8 +146,8 @@ p4 = ggplot(ev_combined[Acq_day %in% latest_n]) +
   theme(axis.text.x = element_text(angle = 90)) +
   facet_grid(LC_meth~Acq_day) +
   theme(legend.position = "none") +
-  coord_cartesian(ylim = c(0,0.6)) +
-  ggtitle(paste0("Peak width per RT section", "( ", n_latest, " latest QCs)")) +
+  coord_cartesian(ylim = c(0,0.4)) +
+  ggtitle(paste0("Peak width per RT section ", "( ", n_latest, " latest QCs)")) +
   xlab("Retention time range")
 p4
 ggsave("QC_Retention_length.pdf", height = length(unique(ev_combined$LC_meth))*3, width = (nruns/4)+3)
@@ -157,7 +158,7 @@ htmlwidgets::saveWidget(ggplotly(p4), file = "QC_Retention_length.html")
 #+ r RT of specific HeLa peptides
 targets_hela = c("DSYVGDEAQSK", "GYSFTTTAER", "TVTAMDVVYALK")
 p5 = ggplot(ev_combined[Sequence %in% targets_hela],
-            aes(x = as.character(Acq_day),
+            aes(x = Acq_day,
                 y = Retention_time,
                 col = paste(Modified_sequence,
                             Charge, m_z),
@@ -178,23 +179,29 @@ htmlwidgets::saveWidget(ggplotly(p5), file = "QC_HeLa_3peptides_RTs.html")
 #'## MaxQuant Summary
 #+ r MaxQuant Summary
 names(summary_combined)
+summary_combined[, Acq_day:=as.character(Acq_day)]
+
 # merge annotation
 summary_combined_ann = merge(summary_combined, ann, by = "Raw_file")
 # write combined csv table
 fwrite(summary_combined_ann, file = "QC_Summary_table.csv")
 # select columns for graphs
 names(summary_combined_ann)
-selected_columns = c(names(summary_combined)[grep("^MS|Peak|Peptide|deviation", names(summary_combined))])
+
+# selected_columns = c(names(summary_combined)[grep("^MS|Peak|Peptide|deviation", names(summary_combined))])
+selected_columns = c(c("MS", "MS_MS", "MS_MS_submitted", "MS_MS_identified", "MS_MS_identified_percent",
+                       "Peptide_sequences_identified", "Peaks", "Peaks_sequenced", "Peaks_sequenced_percent"))
+
 # plot graphs
 pdf("QC_Summary_graphs.pdf", height = 5, width = 3+length(unique(summary_combined_ann$Acq_day))/4)
 for (col in selected_columns) {
   print(col)
-  p = ggplot(summary_combined_ann, aes_string(x = "as.character(Acq_day)", y = col, col = "LC_id")) +
+  p = ggplot(summary_combined_ann, aes_string(x = "as.factor(Acq_day)", y = col, col = "LC_id")) +
     geom_point() +
     geom_line() +
     theme_bw() +
     theme(axis.text.x = element_text(angle = 90)) +
-    facet_wrap(~LC_meth) +
+    facet_grid(~LC_meth) +
     coord_cartesian(ylim = c(0, 1.1*max(summary_combined_ann[[col]]))) +
     ggtitle(paste("MaxQuant Summary: ", col, "@", MSname))
   plot(p)
